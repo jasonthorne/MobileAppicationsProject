7a422ecb6d542e17baed4b67be6ce9fb0dc3554e
JasonThorne <Jason@JAY-PC>
2016-03-23 20:04:19 +0000

    commit 3

--- MobileApplicationsProject/MobileApplicationsProject/MainPage.xaml.cs
+++ MobileApplicationsProject/MobileApplicationsProject/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,14 +23,38 @@ namespace MobileApplicationsProject
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        string message;
+
         public MainPage()
         {
             this.InitializeComponent();
+            
+            folderTest();
+        }
+
+
+        private async void folderTest()
+        {
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+
+            StorageFolder myFolder = await folder.CreateFolderAsync("myFolder", CreationCollisionOption.OpenIfExists);
+
+            StorageFile textFile = await myFolder.CreateFileAsync("textFile.txt", CreationCollisionOption.OpenIfExists);
+            await FileIO.WriteTextAsync(textFile, "Message from file");
+
+            var allFiles = await myFolder.GetFilesAsync();
+
+            StorageFile wantedFile = allFiles.FirstOrDefault(x => x.Name == "textFile.txt");
+
+            message = await FileIO.ReadTextAsync(wantedFile);
+
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            testTextBox.Text = message;
         }
+
     }
 }
